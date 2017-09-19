ed7cf7bf1bd6e2c826867e6bd0352789a9c3a145
Jose Seie <joseeugenioseie@hotmail.com>
2017-09-19 07:26:40 +0200

    User Register modification concluded, And fixed the String lenght bug

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,9 +11,27 @@
                     <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
 
+                        <div class="form-group{{ $errors->has('gender') ? ' has-error' : '' }}">
+                            <label for="gender" class="col-md-4 control-label">Gender</label>
+
+                            
+                            <div class="col-md-6">
+                                <select id="gender"class="form-control" name="gender" value="{{ old('gender') }}">
+                                <option value="1">Male</option>
+                                <option value="0">Female</option>
+                                </select>
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
 
+                            
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
 
